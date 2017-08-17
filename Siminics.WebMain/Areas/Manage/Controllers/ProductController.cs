6b41445b4cbc1658fbda9f60646eae4538062244
赵斌 <urzhaobin@gmail.com>
2017-08-17 21:23:26 +0800

    产品
    产品管理

---
+++ Siminics.WebMain/Areas/Manage/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using Siminics.WebMain.Areas.Manage.Controllers;
+
+namespace Siminics.WebMain.Controllers
+{
+    public class ProductController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
